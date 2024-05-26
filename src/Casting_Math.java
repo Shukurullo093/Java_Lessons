d85fcdf4caad9b742bc1e5c22d2ac7db40282141
Shukurullo093 <nazarqosimovshukurullo093@gmail.com>
2024-05-26 17:55:24 +0500

    #2 Casting & Math

---
+++ src/Casting_Math.java
@@ -0,0 +1,51 @@
+public class Casting_Math {
+    public static void main(String[] args) {
+//        System.out.println("== Byte");
+//        System.out.println(Byte.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println("== Short");
+//        System.out.println(Short.MIN_VALUE);
+//        System.out.println(Short.MAX_VALUE);
+//        System.out.println("== Int");
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println("== Long");
+//        System.out.println(Long.MIN_VALUE);
+//        System.out.println(Long.MAX_VALUE);
+
+//        System.out.println("No.\t Data Type\t Size\t Min. Value\t\t Max. Value\t");
+//        System.out.println("1\t Byte\t\t" + Byte.SIZE
+//                + "\t" + Byte.MIN_VALUE
+//                + "\t\t\t" + Byte.MAX_VALUE);
+//        System.out.println("2\t Short\t\t" + Short.SIZE
+//                + "\t" + Short.MIN_VALUE
+//                + "\t\t\t" + Short.MAX_VALUE);
+//        System.out.println("3\t Integer\t" + Integer.SIZE
+//                + "\t" + Integer.MIN_VALUE
+//                + "\t\t" + Integer.MAX_VALUE);
+//        System.out.println("4\t Float\t\t" + Float.SIZE
+//                + "\t" + Float.MIN_VALUE
+//                + "\t\t\t" + Float.MAX_VALUE);
+//        System.out.println("5\t Long\t\t" + Long.SIZE
+//                + "\t" + Long.MIN_VALUE + "\t"
+//                + Long.MAX_VALUE);
+//        System.out.println("6\t Double\t" + Double.SIZE
+//                + "\t" + Double.MIN_VALUE
+//                + "\t\t" + Short.MAX_VALUE);
+//        System.out.println("7\t Character\t"
+//                + Character.SIZE);
+
+//        float a = 10.0001f;
+//        double b = 10.0001;
+//        System.out.println(a);
+//        System.out.println(b);
+
+        int a1 = 10; //primitive
+        Integer a2 = 10; //non-primitive
+
+        byte byte1 = 10;
+        int int1 = byte1;
+        System.out.println(byte1);
+        System.out.println(int1);
+    }
+}
